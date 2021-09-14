9feacfea8de3547c5185a24fae71db62ece42850
piyushmishra12 <piyushmishra1999@gmail.com>
2021-09-14 15:52:53 +0200

    python code changes

--- tp1/python/hello.py
+++ tp1/python/hello.py
@@ -1,2 +1,13 @@
 name = input("hello! What's you're name\n> ")
 print("Well, it's nice to meet you", name)
+
+
+a=1
+b=a
+a=2
+print(b)
+
+a=[1,2]
+b=a
+a[0]=2
+print(b)
--- tp1/python/main.py
+++ tp1/python/main.py
@@ -1,19 +1,23 @@
 from numpy import random
+import time
 
 MIN_NUMBER = 0
-MAX_NUMBER = 100
+MAX_NUMBER = 5
 
 def game():
     ''' The game in itself    
     '''
     
     # The random number to guess
-    r = random.randint(MIN_NUMBER,MAX_NUMBER)
+    # r = random.randint(MIN_NUMBER,MAX_NUMBER)
     found = False
     
     # (Eternal) loop
-    while not found:
-        
+    while True:
+        r = random.randint(MIN_NUMBER,MAX_NUMBER)
+        name = input("What is your name?")
+        if name == "Piyush":
+            print("Cheating: Enter "+str(r))
         entry = input("\nEnter a number between "+str(MIN_NUMBER)+" and "+str(MAX_NUMBER)+": ")
         while not entry.isdigit():
             print("please input a number")
@@ -21,6 +25,10 @@ def game():
 
         entry = int(entry)
         # Condition on what to do
+        # if entry > MAX_NUMBER or entry < MIN_NUMBER:
+        #     time.sleep(10)
+        #     print("Think on your behaviour")
+        #     return
         if entry == r:
             print("\n\nGood job, it was "+str(r)+"!!!")
             found=True
@@ -31,9 +39,9 @@ def game():
     	
  
 # Start the game only if you wish
-playerwish = input("Hello, want to play a game? ")
-if playerwish in ["yes", "y"] :
-	game()
+# playerwish = input("Hello, want to play a game? ")
+# if playerwish in ["yes", "y"] :
+game()
 	
 
 
